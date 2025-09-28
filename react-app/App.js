770cc12699f249ab87290dd546dce44c95642522
dianannperla <waafirmelyza@hotmail.com>
2025-09-28 13:54:02 +0100

    feat: initial commit for react-app/App.js

---
+++ react-app/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Welcome to the React App</h1>
+      <p>This is a simple application built with React.</p>
+    </div>
+  );
+};
+
+export default App;
