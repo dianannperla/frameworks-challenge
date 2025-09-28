8da9463d1f46bf7901ed9936c57095d81bbb1082
dianannperla <waafirmelyza@hotmail.com>
2025-09-28 13:54:08 +0100

    feat: initial commit for vue-app/src/App.vue

---
+++ vue-app/src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>Welcome to the Vue App</h1>
+    <p>This is a simple application built with Vue.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+};
+</script>
